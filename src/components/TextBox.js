8e15d5e95d38b1638122c132a74c739f03c0f58a
echizenyayota <virtualmisso30@gmail.com>
2023-10-04 22:25:01 +0900

    21:42 style the input and the down-arrow

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -2,7 +2,7 @@ import SelectDropDown from "./SelectDropDown";
 
 const TextBox = ({ style }) => {
   return (
-    <div>
+    <div class={style}>
       <SelectDropDown />
       <textarea 
         placeholder={style === "input" ?  "Enter text" : "Translation"}
